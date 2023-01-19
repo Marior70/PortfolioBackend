40016f7d14fb142f360f58004e23dd965942fa9a
Marior70 <mariolerman@gmail.com>
2023-01-19 11:38:21 -0300

    Agregado capa dto

---
+++ src/main/java/com/portfolio/backend/dto/PersonaDTO.java
@@ -0,0 +1,23 @@
+package com.portfolio.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class PersonaDTO {
+   
+   private Long id;
+   private String nombres;
+   private String apellidos;
+   private String foto;   
+   private String email;
+   private String movil;
+   private String acercade;   
+   private String localidad;   
+   private String titulo;
+
+}
+
+
+
+
+
